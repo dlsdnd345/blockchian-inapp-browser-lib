2e3174cc3450e16b29b844bc56cd60193ce0595e
woong <dlsdnd345@naver.com>
2022-10-27 12:19:50 +0900

    :feat Add 기본 프로젝트 업로드

---
+++ app/src/main/java/co/iw/blockchianinappbrowserlib/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.iw.blockchianinappbrowserlib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
